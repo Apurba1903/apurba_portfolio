17e288419a27cb417f65b5413d99118507b0b3fd
Apurba1903 <apurba1903@gmail.com>
2025-03-04 03:05:43 +0600

    changing repo because of deployment problem

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,180 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+
+
+
+export const About = () => {
+
+    const programmingSkills = [
+        "Python", 
+        "C",
+        "JavaScript",
+        
+    ];
+
+    const frameworkSkills = [
+        "NumPy", 
+        "Pandas",
+        "Matplotlib",
+        "Scikit-learn",
+        "TensorFlow",
+        
+    ];
+
+    const frontendSkills = [
+        "HTML", 
+        "CSS", 
+        "React",
+        "Tailwind CSS",
+    ];
+
+    const backendSkills = [
+        "Node.js", 
+        
+        
+    ];
+
+    return (
+    <section 
+    id="about" 
+    className=" min-h-screen flex items-center justify-center py-20"
+    >       
+        <RevealOnScroll>
+
+        <div className="max-w-3xl mx-auto px-4">
+
+            <h2 className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"
+            >   About Me
+            </h2>
+
+            <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+
+                <p className="text-gray-300 mb-6">
+                    Passionate data science student with a strong foundation in artificial intelligence, machine learning, and big data analytics. My expertise lies in data-driven problem-solving, data visualization, and predictive modeling. 
+                </p>
+                
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4"> Programming Language </h3>
+                        <div className="flex flex-wrap gap-2"> 
+                            {programmingSkills.map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4">  Frameworks / Libraries </h3>
+                        <div className="flex flex-wrap gap-2"> 
+                            {frameworkSkills.map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4"> Frontend </h3>
+                        <div className="flex flex-wrap gap-2"> 
+                            {frontendSkills.map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4"> Backend </h3>
+                        <div className="flex flex-wrap gap-2"> 
+                            {backendSkills.map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+
+
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all"
+                >
+                    <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+                    <ul className="list-disc list-inside text-gray-300 space-y-2">
+                        <li>
+                            <strong>BSc in Computer Science and Engineering</strong> - Bangladesh University of Business and Technology (2020-2024)
+                        </li>
+                        <li>
+                            <strong>Relevant Coursework:</strong> 
+                            
+                            <ol className="list-inside space-y-2" style={{ listStyleType: "lower-roman" }}>
+
+                                <li>Data Science Mentorship Program (DSMP) 2.0 by CampusX - Running                                    
+                                </li>
+
+                                <li>
+                                Post Graduate Diploma in Data Science  (PGDDS) by IBER, <abbr style={{ textDecoration: "none" }} title="United International University">UIU</abbr> & <abbr style={{ textDecoration: "none" }} title="BASIS Institute of Technology & Management">BITM</abbr> - Running
+                                </li>  </ol>                           
+                
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all"
+                >
+                    <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+                        <div className="space-y-4 text-gray-300">
+
+                            <div>
+                                <h4 className="font-semibold"> 
+                                    <strong>Health Care Expert</strong> at SeekMed Care (March, 2024 - December, 2024)</h4>
+                                <p> 
+                                    <ul className="list-disc list-inside">
+                                    <li>Data Management and Excel Work</li>
+                                    <li>Medical Documentation</li>
+                                    <li>Assisted Patients with Hospital procedure</li>
+                                    
+                                    </ul>
+                                </p>
+                            </div>
+
+                            <div>
+                                <h4 className="font-semibold"><strong>Workshop</strong> on AI Career Management (February, 2025)</h4>
+                                <p> 
+                                    Learned about AI Career Management: Skills, Trends and Opportunities.   
+                                </p>
+                            </div>
+
+                        </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+        </RevealOnScroll>
+        
+    </section>    
+    );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,280 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+
+export const Projects = () => {
+
+    return (
+        <section 
+        id="projects" 
+        className="min-h-screen flex items-center justify-center py-20"
+        >
+            <RevealOnScroll>
+            <div className="max-w-5xl mx-auto px-4">
+                    <h2 className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> Featured Projects
+                    </h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        
+
+
+                        {/* Project 1 Start */}
+                        <div className="p-6 rounded-xl border border-white/10  hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px+8px_rgba(59,130,246,0.1)] transition-all"
+                        >   
+                            {/* Project Name */}
+                            <h3 className="text-xl font-bold mb-2 ">Potato Disease Classification of Leaves Using Deep Learning</h3>
+
+                            {/* Project Description */}
+                            <p className="text-gray-400 mb-3"> 
+                                <ul className="list-disc list-inside">
+                                    <li> 
+                                    <strong>Deep Learning-Based Disease Classification:</strong> This project utilizes deep learning models to accurately classify potato leaf diseases into three categories—Potato Early Blight, Potato Late Blight, and Potato Healthy—using a curated dataset of 3,000 images from Kaggle.
+                                    </li>
+                                    <li>
+                                    <strong>Enhancing Agricultural Productivity:</strong> By enabling early detection and precise identification of potato diseases, this study contributes to improved crop management,ultimately supporting global food security and sustainable agriculture.
+                                    </li>
+                                </ul>
+                            </p>
+                            
+                            {/* Project Technologies */}
+                            <div className="flex flex-wrap gap-2 ">
+                                {["Python", "NumPy", "Pandas", "Scikit-learn", "TensorFlow"].map((tech, key) => (
+                                    <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                                ))}
+                            </div>
+                            
+                            {/* Project Image and Link*/}
+                            <div className="flex justify-between items-center ">
+                                <a 
+                                href="https://github.com/Apurba1903/Potato-Disease-Classification-of-Leaves-using-Deep-Learning"
+                                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <span>View Project ➡️</span>
+                                <img 
+                                    src="/images/plantVillage.jpg" 
+                                    alt="Potato Leaves Photo" 
+                                    className="mt-4 object-cover transition-transform duration-300 hover:scale-110 rounded-lg"
+                                /> 
+                                </a>
+                            </div>
+                        </div>
+                        {/* Project 1 End */}
+                        
+
+
+                        {/* Project 2 Start */}
+                        <div className="p-6 rounded-xl border border-white/10  hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px+8px_rgba(59,130,246,0.1)] transition-all"
+                        >   
+                            {/* Project Name */}
+                            <h3 className="text-xl font-bold mb-2 ">Potato Disease Classification of Leaves Using Deep Learning</h3>
+
+                            {/* Project Description */}
+                            <p className="text-gray-400 mb-3"> 
+                                <ul className="list-disc list-inside">
+                                    <li> 
+                                    <strong>Deep Learning-Based Disease Classification:</strong> This project utilizes deep learning models to accurately classify potato leaf diseases into three categories—Potato Early Blight, Potato Late Blight, and Potato Healthy—using a curated dataset of 3,000 images from Kaggle.
+                                    </li>
+                                    <li>
+                                    <strong>Enhancing Agricultural Productivity:</strong> By enabling early detection and precise identification of potato diseases, this study contributes to improved crop management,ultimately supporting global food security and sustainable agriculture.
+                                    </li>
+                                </ul>
+                            </p>
+                            
+                            {/* Project Technologies */}
+                            <div className="flex flex-wrap gap-2 ">
+                                {["Python", "NumPy", "Pandas", "Scikit-learn", "TensorFlow"].map((tech, key) => (
+                                    <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                                ))}
+                            </div>
+                            
+                            {/* Project Image and Link*/}
+                            <div className="flex justify-between items-center ">
+                                <a 
+                                href="https://github.com/Apurba1903/Potato-Disease-Classification-of-Leaves-using-Deep-Learning"
+                                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <span>View Project ➡️</span>
+                                <img 
+                                    src="/images/plantVillage.jpg" 
+                                    alt="Potato Leaves Photo" 
+                                    className="mt-4 object-cover transition-transform duration-300 hover:scale-110 rounded-lg"
+                                /> 
+                                </a>
+                            </div>
+                        </div>
+                        {/* Project 2 End */}
+
+
+
+                        {/* Project 3 Start */}
+                        <div className="p-6 rounded-xl border border-white/10  hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px+8px_rgba(59,130,246,0.1)] transition-all"
+                        >   
+                            {/* Project Name */}
+                            <h3 className="text-xl font-bold mb-2 ">Potato Disease Classification of Leaves Using Deep Learning</h3>
+
+                            {/* Project Description */}
+                            <p className="text-gray-400 mb-3"> 
+                                <ul className="list-disc list-inside">
+                                    <li> 
+                                    <strong>Deep Learning-Based Disease Classification:</strong> This project utilizes deep learning models to accurately classify potato leaf diseases into three categories—Potato Early Blight, Potato Late Blight, and Potato Healthy—using a curated dataset of 3,000 images from Kaggle.
+                                    </li>
+                                    <li>
+                                    <strong>Enhancing Agricultural Productivity:</strong> By enabling early detection and precise identification of potato diseases, this study contributes to improved crop management,ultimately supporting global food security and sustainable agriculture.
+                                    </li>
+                                </ul>
+                            </p>
+                            
+                            {/* Project Technologies */}
+                            <div className="flex flex-wrap gap-2 ">
+                                {["Python", "NumPy", "Pandas", "Scikit-learn", "TensorFlow"].map((tech, key) => (
+                                    <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                                ))}
+                            </div>
+                            
+                            {/* Project Image and Link*/}
+                            <div className="flex justify-between items-center ">
+                                <a 
+                                href="https://github.com/Apurba1903/Potato-Disease-Classification-of-Leaves-using-Deep-Learning"
+                                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <span>View Project ➡️</span>
+                                <img 
+                                    src="/images/plantVillage.jpg" 
+                                    alt="Potato Leaves Photo" 
+                                    className="mt-4 object-cover transition-transform duration-300 hover:scale-110 rounded-lg"
+                                /> 
+                                </a>
+                            </div>
+                        </div>
+                        {/* Project 3 End */}
+
+
+
+                        {/* Project 4 Start */}
+                        <div className="p-6 rounded-xl border border-white/10  hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px+8px_rgba(59,130,246,0.1)] transition-all"
+                        >   
+                            {/* Project Name */}
+                            <h3 className="text-xl font-bold mb-2 ">Potato Disease Classification of Leaves Using Deep Learning</h3>
+
+                            {/* Project Description */}
+                            <p className="text-gray-400 mb-3"> 
+                                <ul className="list-disc list-inside">
+                                    <li> 
+                                    <strong>Deep Learning-Based Disease Classification:</strong> This project utilizes deep learning models to accurately classify potato leaf diseases into three categories—Potato Early Blight, Potato Late Blight, and Potato Healthy—using a curated dataset of 3,000 images from Kaggle.
+                                    </li>
+                                    <li>
+                                    <strong>Enhancing Agricultural Productivity:</strong> By enabling early detection and precise identification of potato diseases, this study contributes to improved crop management,ultimately supporting global food security and sustainable agriculture.
+                                    </li>
+                                </ul>
+                            </p>
+                            
+                            {/* Project Technologies */}
+                            <div className="flex flex-wrap gap-2 ">
+                                {["Python", "NumPy", "Pandas", "Scikit-learn", "TensorFlow"].map((tech, key) => (
+                                    <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                                ))}
+                            </div>
+                            
+                            {/* Project Image and Link*/}
+                            <div className="flex justify-between items-center ">
+                                <a 
+                                href="https://github.com/Apurba1903/Potato-Disease-Classification-of-Leaves-using-Deep-Learning"
+                                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <span>View Project ➡️</span>
+                                <img 
+                                    src="/images/plantVillage.jpg" 
+                                    alt="Potato Leaves Photo" 
+                                    className="mt-4 object-cover transition-transform duration-300 hover:scale-110 rounded-lg"
+                                /> 
+                                </a>
+                            </div>
+                        </div>
+                        {/* Project 4 End */}
+
+
+
+                        {/* Project 5 Start */}
+                        <div className="p-6 rounded-xl border border-white/10  hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px+8px_rgba(59,130,246,0.1)] transition-all"
+                        >   
+                            {/* Project Name */}
+                            <h3 className="text-xl font-bold mb-2 ">Potato Disease Classification of Leaves Using Deep Learning</h3>
+
+                            {/* Project Description */}
+                            <p className="text-gray-400 mb-3"> 
+                                <ul className="list-disc list-inside">
+                                    <li> 
+                                    <strong>Deep Learning-Based Disease Classification:</strong> This project utilizes deep learning models to accurately classify potato leaf diseases into three categories—Potato Early Blight, Potato Late Blight, and Potato Healthy—using a curated dataset of 3,000 images from Kaggle.
+                                    </li>
+                                    <li>
+                                    <strong>Enhancing Agricultural Productivity:</strong> By enabling early detection and precise identification of potato diseases, this study contributes to improved crop management,ultimately supporting global food security and sustainable agriculture.
+                                    </li>
+                                </ul>
+                            </p>
+                            
+                            {/* Project Technologies */}
+                            <div className="flex flex-wrap gap-2 ">
+                                {["Python", "NumPy", "Pandas", "Scikit-learn", "TensorFlow"].map((tech, key) => (
+                                    <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition"
+                                >
+                                    {tech}
+                                </span>
+                                ))}
+                            </div>
+                            
+                            {/* Project Image and Link*/}
+                            <div className="flex justify-between items-center ">
+                                <a 
+                                href="https://github.com/Apurba1903/Potato-Disease-Classification-of-Leaves-using-Deep-Learning"
+                                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <span>View Project ➡️</span>
+                                <img 
+                                    src="/images/plantVillage.jpg" 
+                                    alt="Potato Leaves Photo" 
+                                    className="mt-4 object-cover transition-transform duration-300 hover:scale-110 rounded-lg"
+                                /> 
+                                </a>
+                            </div>
+                        </div>
+                        {/* Project 5 End */}
+
+
+
+                    </div>
+            </div>
+            </RevealOnScroll>
+
+        </section>
+    );
+};
